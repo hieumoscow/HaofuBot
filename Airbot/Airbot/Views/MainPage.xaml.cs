23409d686e1ad839a53e514e18396ea31429918d
Nguyen Nhu Hieu <hieumoscow89@gmail.com>
2017-12-06 17:48:35 +0800

    Added WebView_Navigating & Moved Webview Source into Xaml.cs

--- Airbot/Airbot/Views/MainPage.xaml.cs
+++ Airbot/Airbot/Views/MainPage.xaml.cs
@@ -11,12 +11,26 @@ namespace Airbot.Views
     {
         public static Assembly Assembly = typeof(App).GetTypeInfo().Assembly;
 
+        const string BotUrl = "https://haofu2.azurewebsites.net/";
+
         public MainPage()
         {
             InitializeComponent();
             //var htmlSource = new HtmlWebViewSource();
             //htmlSource.Html = LoadAsset("Airbot.Assets.haofu.html");
             //webview.Source = htmlSource;
+            webview.Source = BotUrl;
+            webview.Navigating += Webview_Navigating;
+        }
+
+        void Webview_Navigating(object sender, WebNavigatingEventArgs e)
+        {
+            
+            if (!e.Url.Equals(BotUrl) && e.Url.StartsWith("http", StringComparison.Ordinal))
+            {
+                Device.OpenUri(new Uri(e.Url));
+                e.Cancel = true;
+            }
         }
 
         public static string LoadAsset(string resId)
